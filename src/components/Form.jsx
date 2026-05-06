23a7b5a8e00c5aed591479daa888806b708a4f89
Pritamx4 <pritamsingh4092005@gmail.com>
2026-05-06 22:11:45 +0530

    feat: add Form component for user input

---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <>
+      <form action="" method="post">
+        <input type="text" name="username" placeholder="Full Name" />
+        <input type="email" name="email" placeholder="Email" />
+        <input type="text" name="message" placeholder="Message" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
