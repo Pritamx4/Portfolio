dc4c690e72d4562c377c7c8b7b5658f26e40df47
Pritamx4 <pritamsingh4092005@gamil.com>
2026-07-14 22:37:19 +0530

    style: update AboutLeft and BottomCapsule components for improved layout and interactivity

--- src/components/AboutLeft.jsx
+++ src/components/AboutLeft.jsx
@@ -1,7 +1,7 @@
 const AboutLeft = () => {
   return (
     <div className="font-body flex h-screen w-1/2 items-center justify-center">
-      <div className="h-110 w-140 border-t-4 border border-[#00FFD1] p-10 shadow-2xl shadow-black/50 backdrop-blur-xl">
+      <div className="h-110 w-140 border-t-4 border border-[#00FFD1] p-10 shadow-2xl shadow-black/50  bg-white/1 backdrop-blur-2xl">
         <h6 className="font-ui uppercase tracking-[0.22em]">[ Operator profile ]</h6>
         <h1 className="font-display text-6xl">PRITAM</h1>
         <h6 className="font-ui mt-4 uppercase tracking-[0.12em]">
@@ -13,17 +13,17 @@ const AboutLeft = () => {
           interctivity.
         </p>
         <div className="mt-6 flex h-24 w-full items-center justify-center border-t-3  border-[#00FFD1]">
-          <div className="flex h-full w-1/3 flex-col items-center justify-center border border-[#00FFD1] bg-[#00FFD1]/5">
+          <div className="flex h-full w-1/3 flex-col items-center justify-center border border-[#00FFD1] bg-white/3">
             <p className="font-ui text-xs uppercase tracking-[0.16em]">exp_duration</p>
             <h1 className="font-bold text-3xl">2+</h1>
             <p className="font-ui mt-2 text-xs uppercase tracking-[0.16em]">Years</p>
           </div>
-          <div className="flex h-full w-1/3 flex-col items-center justify-center border border-[#00FFD1] bg-[#00FFD1]/5">
+          <div className="flex h-full w-1/3 flex-col items-center justify-center border border-[#00FFD1] bg-white/3">
             <p className="font-ui text-xs uppercase tracking-[0.16em]">data_deployed</p>
             <h1 className="font-bold text-3xl">3+</h1>
             <p className="font-ui mt-2 text-xs uppercase tracking-[0.16em]">Proj</p>
           </div>
-          <div className="flex h-full w-1/3 flex-col items-center justify-center border border-[#00FFD1] bg-[#00FFD1]/5">
+          <div className="flex h-full w-1/3 flex-col items-center justify-center border border-[#00FFD1] bg-white/3">
             <p className="font-ui text-xs uppercase tracking-[0.16em]">skills_mastered</p>
             <h1 className="font-bold text-3xl">10+</h1>
             <p className="font-ui mt-2 text-xs uppercase tracking-[0.16em]">Skills</p>
--- src/components/BottomCapsule.jsx
+++ src/components/BottomCapsule.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 
 const BottomCapsule = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <div className="fixed bottom-6 left-1/2 -translate-x-1/2 h-20 w-100 rounded-full bg-white/5 backdrop-blur-2xl border border-white/15 shadow-[0_8px_30px_rgba(0,0,0,0.3)] items-center justify-between">
-        
-      </div>
+      <div
+        onClick={() => setOpen(!open)}
+        className={`fixed bottom-6 left-1/2 -translate-x-1/2
+        transition-all duration-300 ease-in-out
+        bg-white/1 backdrop-blur-2xl border border-white/15 ${open ? 'w-96 h-80 rounded-4xl' : 'w-96 h-20 rounded-4xl'}`}
+      ></div>
     </>
   );
 };
