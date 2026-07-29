d846cb440f92f728c27803489ec133fe9f70eef0
Pritamx4 <pritamsingh4092005@gamil.com>
2026-07-29 23:07:51 +0530

    Add fullScreenNav

---
+++ src/components/FullScreenNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div id='fullscreennav' className='h-screen w-screen flex flex-col items-center justify-between'>
+        FullScreenNav
+    </div>
+  )
+}
+
+export default FullScreenNav;
