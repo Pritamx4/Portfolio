3ecb7d1d59c0338781081c590fc5165646d08282
Pritamx4 <pritamsingh4092005@gamil.com>
2026-07-30 18:26:52 +0530

    fix logo visibility

--- src/components/HomeRight.jsx
+++ src/components/HomeRight.jsx
@@ -1,12 +1,13 @@
+import Logo from '../assets/px4 main logo.svg';
 const Right = () => {
   return (
     <div className="lg:h-screen lg:w-1/2 lg:p-40 h-1/2 w-full p-20 text-(--paper)">
       <div className="lg:h-90 lg:w-full border border-(--paper)/10 p-12">
-        <img src="./src/assets/px4 main logo.svg" alt="" className="h-fit w-full " />
+        <img src={Logo} alt="" className="h-fit w-full " />
         {/* 3d model lgana hai */}
       </div>
     </div>
   );
 };
-
+// fix logo visi
 export default Right;
