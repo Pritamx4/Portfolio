982dcdda5bb3cb0cd6bf6e53e038358189df3e7b
Pritamx4 <pritamsingh4092005@gamil.com>
2026-07-30 18:28:49 +0530

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,3 @@
-// import BottomCapsule from './components/BottomCapsule';
-// import FullScreenNav from './components/FullScreenNav';
-// import Navbar from './components/Navbar';
-// import NavContext from './context/NavContext';
-// import About from './pages/About';
-// import Contact from './pages/Contact';
-// import Home from './pages/Home';
-// import Project from './pages/Project';
-
-// const App = () => {
-//   return (
-//     <>
-//       <NavContext>
-//         <Navbar />
-//       <div className="min-h-screen w-full bg-(--ink) text-(--paper)">
-//       <FullScreenNav />
-//         <Home />
-//         <About />
-//         <Project />
-//         <Contact />
-//       </div>
-//       <BottomCapsule />
-//       </NavContext>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
 import BottomCapsule from './components/BottomCapsule';
 import FullScreenNav from './components/FullScreenNav';
 import Navbar from './components/Navbar';
@@ -39,7 +9,7 @@ import Project from './pages/Project';
 import useActiveSection from './hooks/useActiveSection';
 
 const AppContent = () => {
-  useActiveSection(); // yahan call hoga kyunki isko NavbarContext chahiye
+  useActiveSection();
 
   return (
     <>
@@ -50,7 +20,7 @@ const AppContent = () => {
         <About />
         <Project />
         <Contact />
-        <BottomCapsule/>
+        <BottomCapsule />
       </div>
     </>
   );
