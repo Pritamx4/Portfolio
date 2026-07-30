ab617957640f832b458233ce246047ac267f43b1
Pritamx4 <pritamsingh4092005@gamil.com>
2026-07-30 19:09:42 +0530

    fix logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import MenuIcon from './MenuIcon';
 const Navbar = () => {
   return (
     <div className="font-ui fixed  flex top-0 left-0 h-16 w-full justify-between bg-transparent z-101 text-(--paper)">
-      <div className="h-10 w-10 m-2 ">
+      <div className="h-12 w-12 m-2 ">
         <img src={Logo} alt="PX4 logo" className="h-full w-full object-contain" />
       </div>
       <MenuIcon />
