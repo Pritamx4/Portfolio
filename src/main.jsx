55951f8a42e28de0aafd2ecd53f2d7bd0d7a4734
Pritam Singh <pritamsingh4092005@gmail.com>
2026-08-07 14:45:34 +0530

    resolve conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import ReactDom from 'react-dom/client';
 import App from './App.jsx';
 import Lottie from 'lottie-web';
-import {defineElement} from "@lordicon/element";
 import './index.css';
 
-defineElement(Lottie.loadAnimation);
 
 ReactDom.createRoot(document.getElementById('root')).render(<App />);
