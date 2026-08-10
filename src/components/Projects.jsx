5f9e3ac4ae850d78abb37d7307fbf2ae6f22e537
Pritamx4 <pritamsingh4092005@gmail.com>
2026-08-11 02:01:24 +0530

    Add projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+        <div>Projects</div>
+    </div>
+  )
+}
+
+export default Projects;
